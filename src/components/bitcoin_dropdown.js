7ef670a6b19e7f2f9320d15cff3c6aadc6100835
louieSison89 <louie@tomahawk.co.nz>
2020-11-02 00:03:52 +1300

    initial commit

---
+++ src/components/bitcoin_dropdown.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import { ROOT_URL } from '../index';
+
+class BitcoinDropDown extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      term: [],
+      selectedCurrency: '',
+    };
+  }
+  componentDidMount(){
+        axios.get(ROOT_URL).then( (response) => {
+          const options = Object.keys(response.data.bpi);
+          this.setState({
+              term: options
+          })
+        });
+  }
+  _onHandleChange(value) {
+    this.setState({
+       selectedCurrency: value
+    })
+    this.props.onSelectChange(value);
+}
+render(){
+  return(
+       <select
+         onChange={event => this._onHandleChange(event.target.value)}
+        >
+        <option key="default" value="default">Choose Currency</option>
+         {this.state.term.map(currency => {
+         return <option key={currency} value={currency}> {currency} </option>
+         })}
+       </select>
+     );
+  }
+}
+
+export default BitcoinDropDown;
